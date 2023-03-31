76594b1e1c945c07c3964065075b3a55621d5f43
olezh <olezhka1629@gmail.com>
2023-03-31 20:23:28 +0300

    delete Lab3 files

--- ИСП/MauiApp1/MauiApp1/MauiProgram.cs
+++ ИСП/MauiApp1/MauiApp1/MauiProgram.cs
@@ -17,8 +17,7 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 
-		builder.Services.AddTransient<IDbService, SQLiteService>();
-		// builder.Services.AddSingleton<>();
+		
 
 
 
