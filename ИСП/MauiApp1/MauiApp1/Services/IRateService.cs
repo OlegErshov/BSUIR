124f9a2f17bf2a7b3f9d3e817fd4962aef4277ad
olezh <olezhka1629@gmail.com>
2023-04-03 20:22:02 +0300

    started doing 4th lab

---
+++ ИСП/MauiApp1/MauiApp1/Services/IRateService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiApp1.Services.NbrbAPI.Models;
+
+namespace MauiApp1.Services
+{
+    internal interface IRateService
+    {
+        IEnumerable<Rate> GetRates(DateTime date);
+    }
+}
---
+++ ИСП/MauiApp1/MauiApp1/Services/RateService.cs
@@ -0,0 +1,23 @@
+﻿using MauiApp1.Services.NbrbAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Services
+{
+    internal class RateService : IRateService
+    {
+        HttpClient _client;
+        RateService() 
+        {
+            _client = new HttpClient();
+        }
+
+        public IEnumerable<Rate> GetRates(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
